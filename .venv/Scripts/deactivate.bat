378951410960543891a6d7478a9b61e403ef256a
sigaurav <grv.skrwr0562@gmail.com>
2025-05-25 17:57:59 +0530

    Env upload

--- .venv/Scripts/deactivate.bat
+++ .venv/Scripts/deactivate.bat
@@ -1,18 +1,3 @@
-@set VIRTUAL_ENV=
-@set VIRTUAL_ENV_PROMPT=
-
-@REM Don't use () to avoid problems with them in %PATH%
-@if not defined _OLD_VIRTUAL_PROMPT @goto ENDIFVPROMPT
-    @set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
-    @set _OLD_VIRTUAL_PROMPT=
-:ENDIFVPROMPT
-
-@if not defined _OLD_VIRTUAL_PYTHONHOME @goto ENDIFVHOME
-    @set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
-    @set _OLD_VIRTUAL_PYTHONHOME=
-:ENDIFVHOME
-
-@if not defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH
-    @set "PATH=%_OLD_VIRTUAL_PATH%"
-    @set _OLD_VIRTUAL_PATH=
-:ENDIFVPATH
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3b1741ec27972cd55a4ec4fc91f0b16a0603510f64cf84ae0d37117a34845ab
+size 537
